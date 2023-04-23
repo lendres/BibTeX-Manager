19f1a60b026aa31493bcf4cb92501e7cfa371ef9
Ikosy <lendres@fifthrace.com>
2023-04-22 20:56:14 -0500

    Update AssemblyInfo.cs

--- src/BibTeX Manager/Program/AssemblyInfo.cs
+++ src/BibTeX Manager/Program/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("Line Counter")]
-[assembly: AssemblyDescription("A utility for counting lines of text files.")]
+[assembly: AssemblyTitle("BibtexManager")]
+[assembly: AssemblyDescription("A utility for managing BibTeX files.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Digital Production")]
-[assembly: AssemblyProduct("Line Counter")]
+[assembly: AssemblyProduct("Bibtex Manager")]
 [assembly: AssemblyCopyright("Lance A. Endres")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.0.5.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
