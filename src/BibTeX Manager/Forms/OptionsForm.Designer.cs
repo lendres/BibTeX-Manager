da7062838404ae634f9fb329c4a27179d564dd20
Ikosy <lendres@fifthrace.com>
2023-04-29 07:39:50 -0500

    Add options form.

---
+++ src/BibTeX Manager/Forms/OptionsForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BibTeXManager.Forms
+{
+	partial class OptionsForm
+	{
+
+		#region Members
+
+		private System.Windows.Forms.Button buttonCancel;
+		private System.Windows.Forms.Button buttonOK;
+
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		#endregion
+
+		#region Disposing
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#endregion
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonCancel = new System.Windows.Forms.Button();
+			this.buttonOK = new System.Windows.Forms.Button();
+			this.openLastProjectCheckBox = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// buttonCancel
+			// 
+			this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.buttonCancel.Location = new System.Drawing.Point(373, 129);
+			this.buttonCancel.Name = "buttonCancel";
+			this.buttonCancel.Size = new System.Drawing.Size(80, 23);
+			this.buttonCancel.TabIndex = 25;
+			this.buttonCancel.Text = "&Cancel";
+			this.buttonCancel.UseVisualStyleBackColor = true;
+			// 
+			// buttonOK
+			// 
+			this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.buttonOK.Location = new System.Drawing.Point(275, 129);
+			this.buttonOK.Name = "buttonOK";
+			this.buttonOK.Size = new System.Drawing.Size(80, 23);
+			this.buttonOK.TabIndex = 24;
+			this.buttonOK.Text = "&OK";
+			this.buttonOK.UseVisualStyleBackColor = true;
+			this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+			// 
+			// openLastProjectCheckBox
+			// 
+			this.openLastProjectCheckBox.AutoSize = true;
+			this.openLastProjectCheckBox.Location = new System.Drawing.Point(13, 13);
+			this.openLastProjectCheckBox.Name = "openLastProjectCheckBox";
+			this.openLastProjectCheckBox.Size = new System.Drawing.Size(177, 17);
+			this.openLastProjectCheckBox.TabIndex = 26;
+			this.openLastProjectCheckBox.Text = "Open the last project at start up.";
+			this.openLastProjectCheckBox.UseVisualStyleBackColor = true;
+			// 
+			// OptionsForm
+			// 
+			this.AcceptButton = this.buttonOK;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.buttonCancel;
+			this.ClientSize = new System.Drawing.Size(465, 164);
+			this.Controls.Add(this.openLastProjectCheckBox);
+			this.Controls.Add(this.buttonCancel);
+			this.Controls.Add(this.buttonOK);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "OptionsForm";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.CheckBox openLastProjectCheckBox;
+	} // End class.
+} // End namespace.
---
+++ src/BibTeX Manager/Forms/OptionsForm.cs
@@ -0,0 +1,92 @@
+﻿using BibtexManager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BibTeXManager.Forms
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public partial class OptionsForm : Form
+	{
+		#region Members
+
+		#endregion
+
+		#region Construction
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public OptionsForm()
+		{
+			InitializeComponent();
+
+			PopulateControls();
+		}
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Event Handlers
+
+		/// <summary>
+		/// Ok button event handler.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="eventArgs">Event arguments.</param>
+		private void buttonOK_Click(object sender, EventArgs eventArgs)
+		{
+			// TODO: Validation code goes here.
+
+			// Ensure the data is valid.
+			if (!ValidateChildren())
+			{
+				// Tell the user to fix the errors.
+				MessageBox.Show(this, "Errors exist on the form.", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+				// Have to set the DialogResult to none to prevent the form from closing.
+				this.DialogResult = DialogResult.None;
+				return;
+			}
+
+			PushEntriesToDataStructure();
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Initialize the controls with the values from the data structure.
+		/// </summary>
+		protected void PopulateControls()
+		{
+			RegistryAccess registryAccess = Program.Registry;
+
+			this.openLastProjectCheckBox.Checked = registryAccess.LoadLastProjectAtStartUp;
+		}
+
+		/// <summary>
+		/// Save the data back to the data structure.
+		/// </summary>
+		protected void PushEntriesToDataStructure()
+		{
+			RegistryAccess registryAccess = Program.Registry;
+
+			registryAccess.LoadLastProjectAtStartUp = this.openLastProjectCheckBox.Checked;
+		}
+
+		#endregion
+
+	} // End class.
+} // End namespace.
