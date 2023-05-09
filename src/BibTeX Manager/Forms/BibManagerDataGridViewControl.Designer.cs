55d540e6dbe8f67a51343dbd0c5db78a64a90a26
Ikosy <lendres@fifthrace.com>
2023-05-09 17:00:55 -0500

    Add customized DataGridManager.

---
+++ src/BibTeX Manager/Forms/BibManagerDataGridViewControl.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace BibtexManager
+{
+	partial class BibManagerDataGridViewControl
+	{
+		#region Members
+
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		#endregion
+
+		#region Disposing
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#endregion
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BibManagerDataGridViewControl));
+			this.addRawTemplateButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// deleteButton
+			// 
+			this.deleteButton.Location = new System.Drawing.Point(307, 0);
+			// 
+			// addRawTemplateButton
+			// 
+			this.addRawTemplateButton.Image = ((System.Drawing.Image)(resources.GetObject("addRawTemplateButton.Image")));
+			this.addRawTemplateButton.Location = new System.Drawing.Point(371, 0);
+			this.addRawTemplateButton.Name = "addRawTemplateButton";
+			this.addRawTemplateButton.Size = new System.Drawing.Size(40, 30);
+			this.addRawTemplateButton.TabIndex = 7;
+			this.addRawTemplateButton.UseVisualStyleBackColor = true;
+			this.addRawTemplateButton.Click += new System.EventHandler(this.AddRawTemplateButton_Click);
+			// 
+			// BibManagerDataGridViewControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.addRawTemplateButton);
+			this.Name = "BibManagerDataGridViewControl";
+			this.Size = new System.Drawing.Size(569, 31);
+			this.Controls.SetChildIndex(this.insertBelowButton, 0);
+			this.Controls.SetChildIndex(this.deleteButton, 0);
+			this.Controls.SetChildIndex(this.addButton, 0);
+			this.Controls.SetChildIndex(this.moveUpButton, 0);
+			this.Controls.SetChildIndex(this.moveDownButton, 0);
+			this.Controls.SetChildIndex(this.modifyButton, 0);
+			this.Controls.SetChildIndex(this.insertAboveButton, 0);
+			this.Controls.SetChildIndex(this.addRawTemplateButton, 0);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		protected System.Windows.Forms.Button addRawTemplateButton;
+	} // End class.
+} // End namespace.
---
+++ src/BibTeX Manager/Forms/BibManagerDataGridViewControl.cs
@@ -0,0 +1,95 @@
+﻿using BibTeXLibrary;
+using DigitalProduction.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BibtexManager
+{
+	public partial class BibManagerDataGridViewControl : DigitalProduction.Forms.DataGridViewInterfaceControl
+	{
+		#region Fields
+
+		private BibtexProject			_project;
+
+		#endregion
+
+		#region Construction
+
+		public BibManagerDataGridViewControl()
+		{
+			InitializeComponent();
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Use this to add a property to the designer.
+		/// </summary>
+		//[Category("")]
+		public BibtexProject Project { get => _project; set => _project = value; }
+
+		#endregion
+
+		#region Events
+
+		/// <summary>
+		/// Add a new raw bibliography entry based on a template.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="eventArgs">Event arguments.</param>
+		private void AddRawTemplateButton_Click(object sender, EventArgs eventArgs)
+		{
+			SelectBibEntryType selectBibEntryType = new SelectBibEntryType(_project.BibEntryInitialization.TypeNames);
+			DialogResult result = selectBibEntryType.ShowDialog();
+
+			if (result == DialogResult.OK)
+			{
+				BibEntry entry = BibEntry.NewBibEntryTemplate(_project.BibEntryInitialization, selectBibEntryType.SelectedType);
+
+				EditRawBibEntryForm editRawBibEntryForm = new EditRawBibEntryForm((BibtexManagerForm)this.Parent.Parent, this.Project);
+				DialogResultPair dialogResultPair = editRawBibEntryForm.ShowDialog(this, entry, this.Project.WriteSettings);
+
+				if (dialogResultPair.Result == DialogResult.OK)
+				{
+					AddNewObject(dialogResultPair.Object);
+				}
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		#endregion
+
+		#region Protected Button Visibility and Enabling Methods
+
+		/// <summary>
+		/// Sets the location of buttons based on which buttons are hidden or shown.
+		/// </summary>
+		protected override void SetButtonsLocations()
+		{
+			// Subtract 96 for the hidden move buttons and for the hidden insert buttons.
+			int location = 232 - 96*2;
+			this.addRawTemplateButton.Location = new Point(location, 0);
+
+			// Set the location of the remaining buttons.
+			location += 75;
+			this.modifyButton.Location = new Point(location, 0);
+			location += 75;
+			this.deleteButton.Location = new Point(location, 0);
+		}
+
+		#endregion
+
+	} // End class.
+} // End namespace.
