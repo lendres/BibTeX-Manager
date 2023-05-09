c3832a8665ef27fb2f6229ab9b84aacdcf47fde0
Ikosy <lendres@fifthrace.com>
2023-05-09 17:01:17 -0500

    Add SelectBibTempate form.

---
+++ src/BibTeX Manager/Forms/SelectBibEntryTemplate.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace BibtexManager
+{
+	partial class SelectBibEntryType
+	{
+		#region Fields
+		
+		private System.Windows.Forms.ComboBox templateComboBox;
+
+		private System.Windows.Forms.Button cancelButton;
+		private System.Windows.Forms.Button okButton;
+
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		#endregion
+
+		#region Disposing
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#endregion
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cancelButton = new System.Windows.Forms.Button();
+			this.okButton = new System.Windows.Forms.Button();
+			this.templateComboBox = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// cancelButton
+			// 
+			this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.cancelButton.Location = new System.Drawing.Point(242, 56);
+			this.cancelButton.Name = "cancelButton";
+			this.cancelButton.Size = new System.Drawing.Size(80, 23);
+			this.cancelButton.TabIndex = 25;
+			this.cancelButton.Text = "&Cancel";
+			this.cancelButton.UseVisualStyleBackColor = true;
+			// 
+			// okButton
+			// 
+			this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.okButton.Location = new System.Drawing.Point(144, 56);
+			this.okButton.Name = "okButton";
+			this.okButton.Size = new System.Drawing.Size(80, 23);
+			this.okButton.TabIndex = 24;
+			this.okButton.Text = "&OK";
+			this.okButton.UseVisualStyleBackColor = true;
+			this.okButton.Click += new System.EventHandler(this.buttonOK_Click);
+			// 
+			// templateComboBox
+			// 
+			this.templateComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.templateComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.templateComboBox.FormattingEnabled = true;
+			this.templateComboBox.Location = new System.Drawing.Point(13, 13);
+			this.templateComboBox.Name = "templateComboBox";
+			this.templateComboBox.Size = new System.Drawing.Size(309, 21);
+			this.templateComboBox.TabIndex = 26;
+			// 
+			// SelectBibEntryTemplate
+			// 
+			this.AcceptButton = this.okButton;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.cancelButton;
+			this.ClientSize = new System.Drawing.Size(334, 91);
+			this.Controls.Add(this.templateComboBox);
+			this.Controls.Add(this.cancelButton);
+			this.Controls.Add(this.okButton);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "SelectBibEntryTemplate";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+	} // End class.
+} // End namespace.
---
+++ src/BibTeX Manager/Forms/SelectBibEntryTemplate.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace BibtexManager
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public partial class SelectBibEntryType : Form
+	{
+		#region Fields
+
+		#endregion
+
+		#region Construction
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public SelectBibEntryType(List<string> templateNames)
+		{
+			InitializeComponent();
+
+			this.templateComboBox.Items.Clear();
+			this.templateComboBox.Items.AddRange(templateNames.ToArray());
+
+			//PopulateControls();
+		}
+
+		#endregion
+
+		#region Properties
+
+		public string SelectedType { get => this.templateComboBox.SelectedItem as string; }
+
+		#endregion
+
+		#region Event Handlers
+
+		/// <summary>
+		/// Ok button event handler.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="eventArgs">Event arguments.</param>
+		private void buttonOK_Click(object sender, EventArgs eventArgs)
+		{
+			// TODO: Validation code goes here.
+
+			// Ensure the data is valid.
+			if (!ValidateChildren())
+			{
+				// Tell the user to fix the errors.
+				MessageBox.Show(this, "Errors exist on the form.", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+				// Have to set the DialogResult to none to prevent the form from closing.
+				this.DialogResult = DialogResult.None;
+				return;
+			}
+
+			//PushEntriesToDataStructure();
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Initialize the controls with the values from the data structure.
+		/// </summary>
+		protected void PopulateControls()
+		{
+		}
+
+		/// <summary>
+		/// Save the data back to the data structure.
+		/// </summary>
+		protected void PushEntriesToDataStructure()
+		{
+		}
+
+		#endregion
+
+	} // End class.
+} // End namespace.
