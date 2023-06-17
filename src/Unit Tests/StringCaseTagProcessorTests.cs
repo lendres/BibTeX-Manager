4ccc0f2a6f1fbf8a72a1c23832a4f7cf6ec1021f
Ikosy <lendres@fifthrace.com>
2023-06-17 09:00:12 -0500

    Clean up StringTagProcessorTests

--- src/Unit Tests/StringCaseTagProcessorTests.cs
+++ src/Unit Tests/StringCaseTagProcessorTests.cs
@@ -8,9 +8,8 @@ namespace BibtexManagerUnitTests
 	[TestClass]
 	public class StringCaseTagProcessorTests
 	{
-
 		/// <summary>
-		/// Base line test to remove brackets.
+		/// Convert all caps to title case.
 		/// </summary>
 		[TestMethod]
 		public void ConvertAllCapsToTitle()
@@ -19,26 +18,11 @@ namespace BibtexManagerUnitTests
 			string input	= @"THE QUICK BROWN FOX; JUMPED!: OVER THE LAZY DOG.";
 
 			StringCaseTagProcessor processor  = new() { Pattern=@"^[A-Z\s\p{P}]*$",  TagsToProcess = TagsToProcess.All };
-			//StringCaseTagProcessor processor  = new() { Pattern=@"^[A-Z\s\.]+$",  TagsToProcess = TagsToProcess.All };
 
 			BibEntry entry	= new BibEntry() { Title = input };
 			Utilities.RunProcessor(processor, entry);
 
 			Assert.AreEqual(solution, entry.Title);
-		}
-
-		/// <summary>
-		/// Base line test to remove brackets.
-		/// </summary>
-		[TestMethod]
-		public void RemoveEndBracketsWithNewLines()
-		{
-			string replacement    = @"{THE QUICK BROWN FOX: JUMPED; OVER! THE LAZY DOG.}";
-			//string[] splitText      = Regex.Split(replacement, @"(?<=[\s])|\s");
-			string delimeters       = @"\p{P}\s";
-			string[] splitText      = Regex.Split(replacement, @"(?=[" + delimeters + "])|(?<=[" + delimeters + "])");
-
-			splitText      = replacement.Split(new char[] { ' ' });
 		}
 
 	} // End class.
